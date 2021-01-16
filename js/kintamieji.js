4772f34d3eae0f7a3546cb757797a350bcbeaae7
Arturas1989 <76004308+Arturas1989@users.noreply.github.com>
2021-01-16 14:59:41 +0200

    Practice with js

---
+++ js/kintamieji.js
@@ -0,0 +1,57 @@
+const gimimoMetai = 1900;
+
+console.log(5);
+console.log(gimimoMetai);
+const pirmas = 2;
+const antras = 7;
+const suma = pirmas + antras;
+console.log(pirmas, antras, suma);
+
+
+const vardas = 'Chuck';
+console.log(vardas);
+
+const zodis1 = 'Labas';
+const zodis2 = 'rytas';
+
+console.log(zodis1, zodis2);
+
+const sakinys = zodis1 + ' ' + zodis2 + '!';
+console.log(sakinys);
+
+const pavarde = 'Pavardenis';
+const miestas = "Vilnius";
+
+const kabutes1 = "Tekstas su vienguba (') kabute.";
+console.log(kabutes1);
+
+const kabutes2 = 'Tekstas su dviguba \" kabute.';
+console.log(kabutes2);
+
+const kabutes12 = 'Tekstas su viengube (\') ir su dviguba (") kabute.';
+console.log(kabutes12);
+const dabarTamsu = true;
+console.log(dabarTamsu);
+
+const matauAteiti =false;
+console.log(matauAteiti);
+
+const iskritoSesetas = false;
+console.log(iskritoSesetas);
+
+const tusciasSarasas = [];
+console.log(tusciasSarasas);
+
+const pazymiai = [10, 2, 7, 3, 8];
+console.log(pazymiai);
+
+const miestai = ['Anyksciai', 'Birzai', 'Chicago'];
+console.log(miestai)
+
+
+
+
+
+
+
+
